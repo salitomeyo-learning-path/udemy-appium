f7024cb0ff76eacebb546059bea63f1cb2032a5b
Salome Aristizabal Giraldo <salitomeyo16@gmail.com>
2022-09-28 11:43:50 -0500

    feat: assert title comparison

--- src/test/java/poc_appium/Appium/AppiumBasics.java
+++ src/test/java/poc_appium/Appium/AppiumBasics.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
 import io.appium.java_client.AppiumBy;
+import junit.framework.Assert;
 
 
 public class AppiumBasics extends BaseTest {
@@ -16,6 +17,10 @@ public class AppiumBasics extends BaseTest {
 		driver.findElement(By.xpath("//android.widget.TextView[@content-desc='3. Preference dependencies']")).click();
 		driver.findElement(By.id("android:id/checkbox")).click();
 		driver.findElement(By.xpath("(//android.widget.RelativeLayout) [2]")).click();
+		
+		String alertTitle = driver.findElement(By.id("android:id/alertTitle")).getText();
+		Assert.assertEquals(alertTitle, "WiFi settings");
+		
 		driver.findElement(By.id("android:id/edit")).sendKeys("WifiName");
 		driver.findElements(AppiumBy.className("android.widget.Button")).get(1).click();
 	}
