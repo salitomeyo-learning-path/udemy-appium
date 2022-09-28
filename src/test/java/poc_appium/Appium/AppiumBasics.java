e0e6d097f3e483bf5307ac74ff2da0b1c5ad899a
Salome Aristizabal Giraldo <salitomeyo16@gmail.com>
2022-09-28 10:17:30 -0500

    feat: baseTest configuration

--- src/test/java/poc_appium/Appium/AppiumBasics.java
+++ src/test/java/poc_appium/Appium/AppiumBasics.java
@@ -1,37 +1,16 @@
 package poc_appium.Appium;
 
-import java.io.File;
-import java.net.MalformedURLException;
-import java.net.URL;
-
 import org.testng.annotations.Test;
 
-import io.appium.java_client.android.AndroidDriver;
-import io.appium.java_client.android.options.UiAutomator2Options;
-import io.appium.java_client.service.local.AppiumDriverLocalService;
-import io.appium.java_client.service.local.AppiumServiceBuilder;
+import io.appium.java_client.AppiumBy;
 
 
-public class AppiumBasics {
+public class AppiumBasics extends BaseTest {
 
 	@Test
-	public void AppiumTest() {
-		AppiumDriverLocalService service = new AppiumServiceBuilder().withAppiumJS( new File("C://Users//salome.aristizabalg//AppData//Roaming//npm//node_modules//appium//build//lib//main.js"))
-				.withIPAddress("127.0.0.1").usingPort(4723).build();
-		service.start();
-		
-		UiAutomator2Options options = new UiAutomator2Options();
-		options.setDeviceName("AppiumTesting");
-		options.setApp("C://Users//salome.aristizabalg//eclipse-workspace//Appium//src//test//java//resources//ApiDemos-debug.apk");
-		
-		
-		try {
-			AndroidDriver driver = new AndroidDriver(new URL("http://127.0.0.1:4723"), options);
-			driver.quit();
-			service.stop();
-		} catch (MalformedURLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+	public void SettingWifiName() {
+		//Locators appium supports
+		//Xpath, id, accessibilityId, classname, androidUIAutomator
+		driver.findElement(AppiumBy.accessibilityId("Preference")).click();
 	}
 }
