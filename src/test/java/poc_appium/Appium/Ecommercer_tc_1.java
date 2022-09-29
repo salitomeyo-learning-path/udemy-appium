f3168fc38988a41fbdf8c2824011b48cfcd80615
Salome Aristizabal Giraldo <salitomeyo16@gmail.com>
2022-09-29 14:46:53 -0500

    feat: login error message

--- src/test/java/poc_appium/Appium/Ecommercer_tc_1.java
+++ src/test/java/poc_appium/Appium/Ecommercer_tc_1.java
@@ -4,8 +4,21 @@ import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
 import io.appium.java_client.AppiumBy;
+import junit.framework.Assert;
 
 public class Ecommercer_tc_1 extends BaseTest{
+	
+	@Test
+	public void FillFormError() {
+		driver.findElement(By.xpath("//android.widget.RadioButton[@text='Female']")).click();
+		driver.findElement(By.id("android:id/text1")).click();
+		driver.findElement(AppiumBy.androidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"Colombia\"))")).isDisplayed();
+		driver.findElement(By.xpath("//android.widget.TextView[@text='Colombia']")).click();
+		driver.findElement(By.id("com.androidsample.generalstore:id/btnLetsShop")).click();
+		
+		String toastMessage = driver.findElement(By.xpath("(//android.widget.Toast)[1]")).getAttribute("name");
+		Assert.assertEquals(toastMessage, "Please enter your name");
+	}
 
 	@Test
 	public void FillForm() {
